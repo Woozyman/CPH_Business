735d84cee3920fa57a4f3114166082d902b2e04a
Frey Clante <fclante@gmail.com>
2015-09-02 12:38:47 +0200

    Made a Person Class and added state and get/set methods

---
+++ Adressebog/Adressebog/Person.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adressebog
+{
+    class Person
+    {
+
+
+        public static int Id = 0;
+        public PersonState State;
+        private string name = "";
+        private string address = "";
+        private string country = "";
+        private int zipCode = 0;
+        private int personId;
+
+        public Person()
+        {
+            this.personId = ++Id;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public string Adress
+        {
+            get { return address; }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                   address = value;
+                }
+            }
+        }
+
+        public string Country
+        {
+            get
+            {
+                return country;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    country = value;
+                }
+            }
+        }
+
+        public int PersonId
+        {
+            get
+            {
+                return personId;
+            }
+        }
+
+        public int Zipcode
+        {
+            get
+            {
+                return zipCode;
+            }
+            set
+            {
+                this.zipCode = value;
+            }
+        }
+
+    }
+
+    enum PersonState
+    {
+        friend,
+        enemy,
+        happy,
+        sad,
+        hungry,
+        horny
+    };
+}
---
+++ Adressebog/Adressebog/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adressebog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p1 = new Person();
+            p1.Name = "Frey";
+            p1.State = PersonState.happy;
+            Console.WriteLine("id:  {0} Name: {1} State: {2} ", p1.PersonId, p1.Name, p1.State);
+
+
+            Person p2 = new Person();
+            p2.State = PersonState.friend;
+            p2.Name = "Jens";
+            Console.WriteLine("id:  {0} Name: {1} State: {2} ", p2.PersonId, p2.Name, p2.State);
+        }
+    }
+}
