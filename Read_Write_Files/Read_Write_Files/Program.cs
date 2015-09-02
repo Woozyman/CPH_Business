c4f44b965b9ffca007f34f8ad84c09ae5abedd7e
Frey Clante <fclante@gmail.com>
2015-09-02 08:58:34 +0200

    Example Writing and Reading files

---
+++ Read_Write_Files/Read_Write_Files/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("File Write/Read Example");
+
+            StreamWriter Writer = new StreamWriter("TextFile1.txt");
+            Writer.WriteLine("This is my first attempt to write a file!");
+            Writer.Close();
+
+            StreamReader Reader = new StreamReader("TextFile1.txt");
+            string OutputString = Reader.ReadLine();
+            Reader.Close();
+            Console.WriteLine(OutputString);
+        }
+    }
+}
