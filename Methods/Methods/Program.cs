2e9154084953cf59a0c9afe0975369061616bb99
Frey Clante <fclante@gmail.com>
2015-09-07 19:10:04 +0200

    Added AddUp2() Method

--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -11,9 +11,22 @@ namespace Methods
         static void Main(string[] args)
         {
             Console.WriteLine("Type in a number to AddUp() Function: ");
-            int num = Int32.Parse(Console.ReadLine());
+            int num;
+            if(int.TryParse(Console.ReadLine(), out num))
+            
+
+            if(num != 0)
             Console.WriteLine("Result is: {0} ", AddUp(num));
-                                             
+
+
+            Console.WriteLine("Type in a number to AddUp2() Function: ");
+            if (int.TryParse(Console.ReadLine(), out num))
+
+
+                if (num != 0)
+                    Console.WriteLine("Result is: {0} ", AddUp2(num));
+
+
         }
 
         static int AddUp(int n)
@@ -33,6 +46,24 @@ namespace Methods
             }
 
             return result;
+        }
+
+        static int AddUp2(int n)
+        {
+            int res = 0, temp, count = 0;
+
+            temp = n * 2;
+            while(count <= temp)
+            {
+                if (count % 2 == 0)
+                {
+                  // res += count;
+                    res = res + count; // is equivalent
+                }
+                count++;
+            }
+            
+            return res;
         }
     }
 }
