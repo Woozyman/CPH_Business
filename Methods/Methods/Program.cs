017713c37da0ceacabc5022328d861d1d0e7988e
Frey Clante <fclante@gmail.com>
2015-09-07 18:21:44 +0200

    Made the Methods Project. Doing a simple AddUp() Method

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a number to AddUp() Function: ");
+            int num = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Result is: {0} ", AddUp(num));
+                                             
+        }
+
+        static int AddUp(int n)
+        {
+            //int result = 0;
+            //for (int i = 0; i <= n; i++)
+            //    result += i;
+            //return result;
+
+            int result = 0;
+            int counter = 0;
+            while (counter <= n)
+            {
+                result = result + counter;
+                counter++;
+
+            }
+
+            return result;
+        }
+    }
+}
