e6ba0a7fa909c728f4528babf16976a942a1cca4
Frey Clante <fclante@gmail.com>
2015-09-11 18:23:10 +0200

    added more examples to Project.

--- UnderstandingLinq/UnderstandingLinq/Program.cs
+++ UnderstandingLinq/UnderstandingLinq/Program.cs
@@ -19,18 +19,32 @@ namespace UnderstandingLinq
                 new Person() {FirstName = "Jens", LastName = "Kolby", BornYear = 1967, Height = 175, Gender = "Male" }
             };
 
-            //var byGender = from person in myPersons
-            //                  where person.Gender == "Male"
-            //                  select person;
+            var byGender = from person in myPersons
+                           where person.Gender == "Male"
+                           select person;
 
-            var byYearOfBirth = from person in myPersons
-                                orderby person.BornYear descending
-                                select new { person.FirstName, person.BornYear, person.Gender};
+            //var byYearOfBirth = from person in myPersons
+            //                    orderby person.BornYear descending
+            //                    select new { person.FirstName, person.BornYear, person.Gender};
 
-            foreach (var person in byYearOfBirth)
+            //var orderByLenghtOfFirstName = from person in myPersons
+            //                          orderby person.FirstName.Length
+            //                          select person;
+
+            //var methodSyntax = myPersons.Where(p => p.LastName == "Clante").Where(p => p.BornYear == 1984);
+
+            // var methodSyntax = myPersons.Where(p => p.LastName == "Clante");
+
+            //Use foreach() when more than one property is asked about. Else use a Console.Writeline(); like the one further down.
+            foreach (var person in byGender)
             {
                 Console.WriteLine("{0} - {1} - {2}", person.FirstName, person.BornYear, person.Gender);
             }
+
+            //var methodSyntax = myPersons.Sum(p => p.BornYear);
+
+            //Console.WriteLine(methodSyntax);
+
         }
     }
 }
